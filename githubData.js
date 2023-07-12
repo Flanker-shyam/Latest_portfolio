a91b57560f971badeda69ed7a813e3c221d7969c
Flanker-shyam <shyamp665@gmail.com>
2023-07-12 20:46:04 +0530

    fix repo fetching

--- githubData.js
+++ githubData.js
@@ -1,26 +1,40 @@
-
 const fetch = require('node-fetch')
-const user = 'Flanker-shyam'
+require('dotenv').config();
 
-const getUserRepoName = async (url)=>
-{
-    let arr=[];
-    let response = await fetch(url);
-    let result = await response.json();
-    result.forEach((data)=>{
-        if(data.stargazers_count>=2)
-        {
-            arr.push(data.name);
+// GitHub GraphQL API endpoint
+const endpoint = 'https://api.github.com/graphql';
+
+// Your GitHub Personal Access Token
+const accessToken = process.env.GITHUB_ACCESS_TOKEN;
+
+// GraphQL query to retrieve pinned repositories
+const query = `
+  query {
+    user(login: "Flanker-shyam") {
+      pinnedItems(first: 5, types: REPOSITORY) {
+        edges {
+          node {
+            ... on Repository {
+              name
+              description
+              url
+            }
+          }
         }
-    })
-    var ans = await getRepoData(arr);
-    return ans;
-}
+      }
+    }
+  }
+`;
 
+// Define the request headers including the Authorization header
+const headers = {
+  'Authorization': `Bearer ${accessToken}`,
+  'Content-Type': 'application/json'
+};
 
 async function getRepoData(arr1)
 {
-    var fordata=[];
+    let fordata=[];
     for(let name1 of arr1){
         let dataHere={};
         let res = await fetch('https://api.github.com/repos/Flanker-shyam/'+name1);
@@ -36,4 +50,36 @@ async function getRepoData(arr1)
     return fordata;
 }
 
+const getUserRepoName = async ()=>
+{
+    let arr=[];
+    // Construct the POST request body
+    const body = JSON.stringify({ query });
+
+    // Send the POST request to the GitHub GraphQL API
+    await fetch(endpoint, {
+    method: 'POST',
+    headers,
+    body
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Extract the pinned repositories from the response
+        const pinnedRepos = data.data.user.pinnedItems.edges.map(edge => edge.node);
+        
+        // Display the repository information
+        pinnedRepos.forEach(repo => {
+            arr.push(repo.name);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching pinned repositories:', error);
+    });
+    let ans = await getRepoData(arr);
+    // console.log(ans);
+    return ans;
+}
+
+
+getUserRepoName();
 module.exports = getUserRepoName;
